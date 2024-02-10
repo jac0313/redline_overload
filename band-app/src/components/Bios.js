b3da8216a4ddf01b04142ff82b4c990999581072
jac0313 <amelia.castaneda@ymail.com>
2024-02-10 09:17:18 -0700

    adding files

--- band-app/src/components/Bios.js
+++ band-app/src/components/Bios.js
@@ -2,6 +2,7 @@ import Hero from './Hero'
 import Card from 'react-bootstrap/Card';
 import { Container, Row, Col } from 'react-bootstrap'
 
+
 function Bios(){
     return(
         <>
@@ -10,12 +11,11 @@ function Bios(){
         <Row>
             <Col>
                 <Card className='mb-5' style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="/Users/amelia/Desktop/redline_overload/band-app/src/images/catherine.jpeg" />
+                    <Card.Img variant="top" src="band-app/src/images/catherine.jpeg" />
                     <Card.Body>
-                        <Card.Title>Catherine</Card.Title>
+                        <Card.Title>Kathryn</Card.Title>
                         <Card.Text>
-                         Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                         Kathryn is the dynamic vocalist and front woman for Redline Overload. She brings the crowds to their feet with energetic delivery, beautiful vocals and her blonde bombshell aesthetic. This she-wolf fuels her performances with the rage and fury that can only be harnessed by a mother of three boys.
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -26,8 +26,7 @@ function Bios(){
                     <Card.Body>
                         <Card.Title>Trent</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Trent is Redline Overload's lead guitarist. He drops panties and breaks hearts with his soloing skills. Trent deploys the famous Joe Satriani technique of shaving his head for maximum aerodynamic playing along the fretboard. Although you wouldn't guess from from his glamorous presentation, Trent likes to wind down humbly with a sixer of his favorite beer and Totino's pizza rolls.
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -38,10 +37,9 @@ function Bios(){
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="/Users/amelia/Desktop/redline_overload/band-app/src/images/sarah.jpeg" />
                     <Card.Body>
-                        <Card.Title>Sarah</Card.Title>
+                        <Card.Title>Sara</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Sara is Redline's drummer and beating heart. This horse riding, hockey playing, renaissance girl started playing drums just a couple years ago, probably on a dare, and is quickly rising the ranks of the worlds most influential drummers, earning follows and recognition from national and international acts. The defacto band manager is also venturing into the sound engineering and production space because her curiosity can't be satiated. Better come catch her as Redline's drummer before she get's bored and starts training to become an astronaut or something.  
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -52,8 +50,7 @@ function Bios(){
                     <Card.Body>
                         <Card.Title>Fernando</Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        Fernando is Redline's newest addition and is featured on bass guitar. In their search for a new bass player, the band discovered Fernando at an insane asylum for deluded musicians and were charmed with his infectious playing style, winning personality, and devilish good looks. He's always ready to bring the funk and the pain so long as he's not dying from the flu and coughing his motherfucking spleen out.
                         </Card.Text>
                     </Card.Body>
                 </Card>
